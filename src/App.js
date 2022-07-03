2399c6dadc7523ffd2856005902caa6f7e29b44a
Andrew Wand <andrewwands@gmail.com>
2022-07-02 21:23:47 -0500

    set movie images. need better way to do it tho.

--- src/App.js
+++ src/App.js
@@ -21,42 +21,58 @@ function App() {
       <Header />
       <Main />
       
-      <div className="carousel-one">
+<div className="carousel-container">
+  <div className="carousel-one">
       <Carousel carouselTitle="Continue Watching" >
-        <CarouselItem img='assets/images/avengers-endgame.jpg' />
-        <CarouselItem  img='assets/images/avengers-endgame.jpg' />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
+        <CarouselItem img='assets/images/movies/breaking-bad.jpg' />
+        <CarouselItem  img='assets/images/movies/constantine.jpg' />
+        <CarouselItem   img='assets/images/movies/orange-new.jpg'/>
+        <CarouselItem   img='assets/images/movies/spartacus.jpg'/>
+        <CarouselItem  img='assets/images/movies/fma.jpg'/>
+        <CarouselItem  img='assets/images/movies/ahs.jpeg'/>
+        <CarouselItem  img='assets/images/movies/jaws.jpg'/>
+        <CarouselItem  img='assets/images/movies/ghostbusters.jpg'/>
+        <CarouselItem  img='assets/images/movies/fandf.jpg'/>
+        <CarouselItem  img='assets/images/movies/ice-age.jpg'/>
+        <CarouselItem  img='assets/images/movies/orange-new.jpg'/>
+        <CarouselItem  img='assets/images/movies/orange-new.jpg'/>
       </Carousel>
       </div>
       <div className="carousel-two">
       <Carousel carouselTitle="What's New" >
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
+        <CarouselItem img='assets/images/movies/constantine.jpg' />
+        <CarouselItem  img='assets/images/movies/breaking-bad.jpg' />
+        <CarouselItem   img='assets/images/movies/ghostbusters.jpg'/>
+        <CarouselItem   img='assets/images/movies/ice-age.jpg'/>
+        <CarouselItem  img='assets/images/movies/fma.jpg'/>
+        <CarouselItem  img='assets/images/movies/ahs.jpeg'/>
+        <CarouselItem  img='assets/images/movies/jaws.jpg'/>
+        <CarouselItem  img='assets/images/movies/spartacus.jpg'/>
+        <CarouselItem  img='assets/images/movies/fandf.jpg'/>
+        <CarouselItem  img='assets/images/movies/orange-new.jpg'/>
+        <CarouselItem  img='assets/images/movies/orange-new.jpg'/>
+        <CarouselItem  img='assets/images/movies/orange-new.jpg'/>
       </Carousel>
       </div>
       <div className="carousel-three" >
       <Carousel carouselTitle="Your Favorites" >
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
-        <CarouselItem  />
+      <CarouselItem img='assets/images/movies/ice-age.jpg' />
+        <CarouselItem  img='assets/images/movies/constantine.jpg' />
+        <CarouselItem   img='assets/images/movies/spartacus.jpg'/>
+        <CarouselItem   img='assets/images/movies/orange-new.jpg'/>
+        <CarouselItem  img='assets/images/movies/fma.jpg'/>
+        <CarouselItem  img='assets/images/movies/fandf.jpg'/>
+        <CarouselItem  img='assets/images/movies/jaws.jpg'/>
+        <CarouselItem  img='assets/images/movies/ghostbusters.jpg'/>
+        <CarouselItem  img='assets/images/movies/ahs.jpeg'/>
+        <CarouselItem  img='assets/images/movies/breaking-bad.jpg'/>
+        <CarouselItem  img='assets/images/movies/orange-new.jpg'/>
+        <CarouselItem  img='assets/images/movies/orange-new.jpg'/>
       </Carousel>
       </div>
+</div>
+
+     
     <Footer />
       
     </div>
